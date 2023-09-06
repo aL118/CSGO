d4bfee01130b9e64de78da103ac7e9b1f70ce05c
Laura Zheng <lyzheng@umd.edu>
2023-09-06 18:34:29 -0400

    tensors

--- playground/pytorch-test.py
+++ playground/pytorch-test.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import numpy as np
 
 device = (
     "cuda"
@@ -132,6 +133,75 @@ def load_model():
             predicted, actual = classes[pred[0].argmax(0)], classes[y]
             print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
-load_model()
+def tensor_intro():
+    data = [[1, 2],[3, 4]]
+    x_data = torch.tensor(data)
+    print(x_data)
+    np_array = np.array(data)
+    x_np = torch.from_numpy(np_array)
+    print(x_np)
+    x_ones = torch.ones_like(x_data) # retains the properties of x_data
+    print(f"Ones Tensor: \n {x_ones} \n")
+
+    x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+    print(f"Random Tensor: \n {x_rand} \n")
+
+    shape = (2,3)
+    rand_tensor = torch.rand(shape)
+    ones_tensor = torch.ones(shape)
+    zeros_tensor = torch.zeros(shape)
+
+    print(f"Random Tensor: \n {rand_tensor} \n")
+    print(f"Ones Tensor: \n {ones_tensor} \n")
+    print(f"Zeros Tensor: \n {zeros_tensor}")
+
+    tensor = rand_tensor
+
+    print(f"Shape of tensor: {tensor.shape}")
+    print(f"Datatype of tensor: {tensor.dtype}")
+    print(f"Device tensor is stored on: {tensor.device}")
+
+    # We move our tensor to the GPU if available
+    if torch.cuda.is_available():
+        tensor = tensor.to("cuda")
+
+    tensor = torch.ones(4, 4)
+    print(f"First row: {tensor[0]}")
+    print(f"First column: {tensor[:, 0]}")
+    print(f"Last column: {tensor[..., -1]}")
+    print(f"Last column 2a: {x_data[:, -1]}")
+    print(f"Last column 2b: {x_data[..., -1]}")
+    tensor[:,1] = 0
+
+    t1 = torch.cat([tensor, tensor, tensor], dim=1)
+    print(t1)
+
+    # This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+    # ``tensor.T`` returns the transpose of a tensor
+    y1 = tensor @ tensor.T
+    print(y1)
+    y2 = tensor.matmul(tensor.T)
+    print(y2)
+    y3 = torch.rand_like(y1)
+    torch.matmul(tensor, tensor.T, out=y3)
+    print(y3)
+
+    # This computes the element-wise product. z1, z2, z3 will have the same value
+    z1 = tensor * tensor.T
+    print(z1)
+    z2 = tensor.mul(tensor)
+    print(z2)
+    z3 = torch.rand_like(tensor)
+    torch.mul(tensor, tensor, out=z3)
+    print(z3)
+
+    agg = tensor.sum()
+    agg_item = agg.item()
+    print(agg_item, type(agg_item))
+
+    print(f"{tensor} \n")
+    tensor.add_(5)
+    print(tensor)
+tensor_intro()
 
 # code from: https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
